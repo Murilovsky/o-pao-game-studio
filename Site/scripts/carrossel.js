0ffc9b3d07cc138fe880da77ac2e7362829bedfa
Murilo Gama <53795576+Murilovsky@users.noreply.github.com>
2022-09-27 18:30:18 -0300

    Scrollbar

--- Site/scripts/carrossel.js
+++ Site/scripts/carrossel.js
@@ -1,15 +1,15 @@
 const carrossel = document.getElementById("carrossel")
 const paginas = document.querySelectorAll('.game_card')
 
-let idPagina=0;
+let idPagina = 0;
 
-function RodaCarrossel(){
+function RodaCarrossel() {
     idPagina++
     console.log("Carrosel")
-    if (idPagina >= paginas.length){
+    if (idPagina >= paginas.length) {
         idPagina = 0
     }
     carrossel.style.transform = `translateX(${(-idPagina * carrossel.clientWidth)}px)`
 }
 
-setInterval(RodaCarrossel,5700)
+setInterval(RodaCarrossel, 5700)
